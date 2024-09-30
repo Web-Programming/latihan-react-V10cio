1bda3152f4154d89a39652d35f8e37637dada2af
V10cio <161033040+V10cio@users.noreply.github.com>
2024-09-30 22:24:29 +0700

    update

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,39 +1,46 @@
 // import logo from './logo.svg';
-import { Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Hello from './Hello';
+import Product from './Product';
+import Registrasi from './Registrasi';
 import React from 'react';
+import routes from './route';
 
 function App() {
   return (
     <React.Suspense fallback={<div>Loading ....</div>}>
       <div className='App'>
-        <h1>Universitas MDP</h1>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/product">Product</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/register">Registasi</a>
-                </li>
-               
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <div className='main container'>
-          <Routes>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <NavLink to="/" className={"Navbar-brand" }>
+          Navbar
+        </NavLink>
+        <button className='navbar-toggler' type=''>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id='navbarNav'>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/product" className="nav-link">Product</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/register" className="nav-link">Register</NavLink>
+        </li>
+        
+      </ul>
+      </div>
+        
+      </nav>
+
+        <div className='main'>
+        <Routes>
             {
-              Routes.map((route, i)=>{
+              routes.map((route, i)=>{
                 const {path, Component} = route;
                 return <Route key ={i} path = {path} element = {<Component />} />;
               })
