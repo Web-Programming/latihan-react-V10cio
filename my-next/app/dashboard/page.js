5b32fdb5ce00957ff385c46dfc62bb1f9cc3b472
V10cio <161033040+V10cio@users.noreply.github.com>
2024-10-10 20:02:17 +0700

    half

--- my-next/app/dashboard/page.js
+++ my-next/app/dashboard/page.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Page() {
-    return;
-    (
+    return(
         <div>
             <h1>This is Dashboard Page</h1>
+
+            &bull;<Link href="dashboard/users/Valencio Arjun/2327250049">Valencio Arjun</Link>
+            &bull;<Link href="dashboard/users/Cio">Cio</Link>
+
         </div>
     );
   }
